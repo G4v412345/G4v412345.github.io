3a4ef1ff7a6f9070136d1c1e658d7e8747f0dc3c
Mykhailo <131289952+G4v412345@users.noreply.github.com>
2024-11-07 12:22:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
 const URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTslcJFOhtuuGhCACdPhCADrq0gBHG1bKj6JcYXsZKCupOkJlM6MyDonSz1RAGXvUMqZ6wipILxHI6A/pub?gid=1619251371&single=true&output=tsv';
 
-const  SEP_LINE = '\r\n';
-const  SEP_CELL = '\t';
-const  DOM_TABLE = document.querySelector('.price-table table');
+const SEP_LINE = '\r\n';
+const SEP_CELL = '\t';
+const DOM_TABLE = document.querySelector('.price-table table');
 
-function loadDate() 
-{
+function loadDate() {
     fetch(URL).then(r => r.text()).then(parseData);
 }
 
-function parseData(d)
-{
+function parseData(d) {
     const tableArr = d.split(SEP_LINE).map(r => r.split(SEP_CELL));
     const names = tableArr.shift();
 
-    const formatDate = tableArr.map(e1 =>
-        {
-            const name = {};
-            names.forEach((nam, i) => name[nam] = e1[i]);
-
-            return name;
-        }
-    );
+    const formatDate = tableArr.map(e1 => {
+        const name = {};
+        names.forEach((nam, i) => name[nam] = e1[i]);
+        return name;
+    });
 
     renderDate(formatDate, names);
 }
 
-function renderDate(data, names)
-{
+function renderDate(data, names) {
     console.log(data);
     const html = `
         ${getTableHead(names)}
@@ -39,28 +33,27 @@ function renderDate(data, names)
     DOM_TABLE.innerHTML = html;
 }
 
-function getTableHead([order, name, price])
-{
+function getTableHead(names) {
+    // Генерация строки заголовка для каждой колонки
+    const headerRow = names.map(name => `<th scope="col">${name}</th>`).join('');
     return `
-                  <tr>
-                    <th scope="col">$</th>
-                    <th scope="col">${name}</th>
-                    <th scope="col">${price}</th>
-                </tr>
+        <thead>
+            <tr>
+                ${headerRow}
+            </tr>
+        </thead>
     `;
 }
 
-function getTableBody(data)
-{
-    return data.map(({order, name, price}) => {
+function getTableBody(data) {
+    return data.map(row => {
         return `
-        <tr>
-            <th scope="row">${order}</th>
-            <td>${name}</td>
-            <td>${price}</td>
-        </tr>`;
-        }).join('');
-        
+            <tr>
+                <th scope="row">${row.order}</th>
+                <td>${row.name}</td>
+                <td>${row.price}</td>
+            </tr>`;
+    }).join('');
 }
 
 loadDate();
