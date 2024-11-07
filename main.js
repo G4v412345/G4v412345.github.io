e14b9556a133b5a55953f1b687725355c6b44bc9
Mykhailo <131289952+G4v412345@users.noreply.github.com>
2024-11-07 12:25:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,44 @@
 const URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTslcJFOhtuuGhCACdPhCADrq0gBHG1bKj6JcYXsZKCupOkJlM6MyDonSz1RAGXvUMqZ6wipILxHI6A/pub?gid=1619251371&single=true&output=tsv';
 
-const SEP_LINE = '\r\n';
+const SEP_LINE = '\n';
 const SEP_CELL = '\t';
 const DOM_TABLE = document.querySelector('.price-table table');
 
-function loadDate() {
-    fetch(URL).then(r => r.text()).then(parseData);
+function loadData() {
+    fetch(URL)
+        .then(response => response.text())
+        .then(data => parseData(data))
+        .catch(error => console.error('Error loading data:', error));
 }
 
-function parseData(d) {
-    const tableArr = d.split(SEP_LINE).map(r => r.split(SEP_CELL));
-    const names = tableArr.shift();
+function parseData(data) {
+    const rows = data.trim().split(SEP_LINE);
+    const headers = rows[0].split(SEP_CELL);  
+    const bodyRows = rows.slice(1).map(row => row.split(SEP_CELL));  /
 
-    const formatDate = tableArr.map(e1 => {
-        const name = {};
-        names.forEach((nam, i) => name[nam] = e1[i]);
-        return name;
-    });
-
-    renderDate(formatDate, names);
+    renderTable(headers, bodyRows);
 }
 
-function renderDate(data, names) {
-    console.log(data);
-    const html = `
-        ${getTableHead(names)}
-        <tbody>
-        ${getTableBody(data)}
-        </tbody>
-    `;
-
-    DOM_TABLE.innerHTML = html;
-}
-
-function getTableHead(names) {
-    // Генерация строки заголовка для каждой колонки
-    const headerRow = names.map(name => `<th scope="col">${name}</th>`).join('');
-    return `
+function renderTable(headers, bodyRows) {
+    const tableHeadHTML = `
         <thead>
             <tr>
-                ${headerRow}
+                ${headers.map(header => `<th scope="col">${header}</th>`).join('')}
             </tr>
         </thead>
     `;
+
+    const tableBodyHTML = `
+        <tbody>
+            ${bodyRows.map(row => `
+                <tr>
+                    ${row.map(cell => `<td>${cell}</td>`).join('')}
+                </tr>
+            `).join('')}
+        </tbody>
+    `;
+
+    DOM_TABLE.innerHTML = tableHeadHTML + tableBodyHTML;
 }
 
-function getTableBody(data) {
-    return data.map(row => {
-        return `
-            <tr>
-                <th scope="row">${row.order}</th>
-                <td>${row.name}</td>
-                <td>${row.price}</td>
-            </tr>`;
-    }).join('');
-}
-
-loadDate();
+loadData();
